79fb0facd4763a9716fdaa65e8119f668af50ad1
vanhtnv <100839898+vanhtnv@users.noreply.github.com>
2022-03-18 17:52:37 +0700

    Add files via upload
    Thêm các hàm trong class

--- All_Class.cpp
+++ All_Class.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <windows.h>
 using namespace std;
 
 struct teacher
@@ -32,6 +33,14 @@ public:
 private:
 	teacher Teacher;
 };
+void gotoxy(int x, int y)
+{
+    static HANDLE h = NULL;
+    if(!h)
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD c = { x, y };
+    SetConsoleCursorPosition(h,c);
+}
 class Course_Result : public Course
 {
 public:
@@ -41,7 +50,7 @@ public:
 };
 class person
 {
-private:
+protected:
 	string ID;
 	string password;
 	string name;
@@ -53,21 +62,105 @@ private:
 
 public:
 	void input();
-	void input_file();
+	void input_file(string path);
 	void output();
 	void edit_profile();
+
+	string GetPassword();
+	string GetID();
+	string GetName();
+	string GetGender();
+	string GetBirth();
+	void ChangePassword(string NewPassword);
 };
 void person::input()
 {
+    ifstream file;
+    cout << "Name: ";
+    getline(cin, name);
+    cout << "Address: ";
+    getline(cin, address);
+    while (true)
+    {
+        cout << "ID: ";
+        cin >> ID;
+        if (FolderExists(("sinhvien\\" + ID).c_str()))
+        {
+            cout << "ID exists. Try again!" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << "social ID: ";
+    cin >> socialID;
+    cout << "Gender: ";
+    cin >> gender;
+    cout << "Birth: ";
+    cin >> birth;
+    cout << "Phone number: ";
+    cin >> phone;
+    password = "123456";
+
 }
-void person::input_file()
+void person::input_file(string path)
 {
+    ifstream filein;
+    filein.open(path, ios_base::in);
+    getline(filein, password);
+    getline(filein, name, ',');
+    getline(filein, ID, ',');
+    getline(filein, gender, ',');
+    getline(filein, birth, ',');
+    getline(filein, socialID, ',');
+    getline(filein, phone, ',');
+    getline(filein, address);
+    filein.close();
+
 }
 void person::output()
 {
+    gotoxy(45, 5); cout << "Name : " << name;
+    gotoxy(45, 7); cout << "Gender : " << gender;
+    gotoxy(45, 9); cout << "ID: " << ID;
+    gotoxy(45, 11); cout << "Address: " << address;
+    gotoxy(45, 13); cout << "Socail ID: " << socialID;
+    gotoxy(45, 15); cout << "Date of birth: " << birth;
+    gotoxy(45, 17); cout << "Phone number: " << phone;
+    gotoxy(45, 19); cout << "Password: " << password;
+    //Setcolor(7, 0);
+    gotoxy(45, 21); cout << "ESC TO GO BACK. ";
+    //Setcolor(0, 7);
+
 }
 void person::edit_profile()
 {
+}
+
+string person::GetPassword()
+{
+    return password;
+}
+string person::GetID()
+{
+    return ID;
+}
+string person::GetName()
+{
+    return name;
+}
+string person::GetGender()
+{
+    return gender;
+}
+string person::GetBirth()
+{
+    return birth;
+}
+void person::ChangePassword(string NewPassword)
+{
+    password = NewPassword;
 }
 class student : public person
 {
@@ -78,9 +171,16 @@ public:
 	void delete_course(fstream &course_registered);
 	void view_registered_course(fstream course_registered);
 	void view_course_member(fstream course_registered);
+	void SaveFile(string path);
+	string GetClass();
+	void Input();
+    void Input_File(string path);
+    void Input_File2(ifstream &filein);
+    void Output();
 
 private:
 	void view_results(fstream result_list); // b/c 1 student only see his/her results other can't
+	string CLASS;
 };
 void student::class_list(fstream class_list)
 {
@@ -105,6 +205,64 @@ void student::view_results(fstream result_list)
 {
 	// result list is the list that academic staff or teacher will bring
 }
+void student::SaveFile(string path)
+{
+    ofstream fileout;
+    fileout.open(path, ios::out);
+    fileout << password << endl;
+    fileout <<CLASS<<","<< name << "," << ID << "," << gender << "," <<  birth << "," << socialID << "," << phone << "," << address;
+    fileout.close();
+}
+string student::GetClass()
+{
+    return CLASS;
+}
+void student::Input()
+{
+    ifstream fileinput;
+    person::input();
+	while (true)
+	{
+		cout << "Class to add: ";
+		cin >> CLASS;
+		fileinput.open(CLASS + ".txt", ios::in);
+		if (fileinput.fail())
+		{
+			cout << "There is not this class. Try Again!" << endl;
+		}
+		else
+		{
+			fileinput.close();
+			break;
+		}
+		fileinput.close();
+	}
+}
+void student::Input_File(string path)
+{
+    ifstream filein;
+    filein.open(path, ios_base::in);
+    getline(filein, password);
+    getline(filein, CLASS,',');
+    getline(filein, name, ',');
+    getline(filein, ID, ',');
+    getline(filein, gender, ',');
+    getline(filein, birth, ',');
+    getline(filein, socialID, ',');
+    getline(filein, phone, ',');
+    getline(filein, address);
+    filein.close();
+}
+void student::Input_File2(ifstream &filein)
+{
+
+}
+void student::Output()
+{
+    gotoxy(45, 3); cout << "Class : " << CLASS;
+    person::output();
+}
+
 class staff : public person
 {
 public:
